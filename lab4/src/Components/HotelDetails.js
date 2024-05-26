e2eafe535d4b2e13149583bd1592bd7fb4331179
Aleksandra Chrustek <106326730+aleksandrachrustek@users.noreply.github.com>
2024-05-26 20:51:26 +0200

    Update HotelDetails.js

--- lab4/src/Components/HotelDetails.js
+++ lab4/src/Components/HotelDetails.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import hotelData from './hotelData'; // Importuj dane hotelowe
+import hotelData from './hotelData';
 
 const HotelDetails = () => {
   const { id } = useParams();
